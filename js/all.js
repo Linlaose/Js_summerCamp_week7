e2df45b77b5a04dd7edcd88eddd0c422ce6bee08
Ryan Tsai <a0986575797@gmail.com>
2022-11-17 01:58:07 +0800

    Lv3 作業

--- js/all.js
+++ js/all.js
@@ -1,3 +1,6 @@
+import axios from '../node_modules/axios/dist/esm/axios.js';
+import { addChart } from './chart.js';
+
 const ticketName = document.querySelector('#ticketName');
 const ticketImgUrl = document.querySelector('#ticketImgUrl');
 const ticketRegion = document.querySelector('#ticketRegion');
@@ -15,84 +18,59 @@ const searchResult = document.querySelector('#searchResult-text');
 
 const noneArea = document.querySelector('.cantFind-area');
 
-let filter = "";
-let filterData = [];
+const dataUrl = "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
+
+let data = [];
 let id = 3; // 因初始資料的 id 到 2 了，所以新資料的 id 從 3 開始給
 
-let data = [
-  {
-    "id": 0,
-    "name": "肥宅心碎賞櫻3日",
-    "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    "area": "高雄",
-    "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    "group": 87,
-    "price": 1400,
-    "rate": 10
-  },
-  {
-    "id": 1,
-    "name": "貓空纜車雙程票",
-    "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    "area": "台北",
-    "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    "group": 99,
-    "price": 240,
-    "rate": 2
-  },
-  {
-    "id": 2,
-    "name": "台中谷關溫泉會1日",
-    "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    "area": "台中",
-    "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    "group": 20,
-    "price": 1765,
-    "rate": 7
-  }
-];
 
-
-renderCard(data);
-function renderCard(renderData) { // 渲染資料
-  let card = "";
-  renderData.forEach((item) => {
-    card += `
-      <li class="ticketCard">
-        <div class="ticketCard-img">
-          <a href="#">
-            <img
-              src="${item.imgUrl}"
-              alt=""
-            />
-          </a>
-          <div class="ticketCard-region">${item.area}</div>
-          <div class="ticketCard-rank">${item.rate}</div>
-        </div>
-        <div class="ticketCard-content">
-          <div>
-            <h3>
-              <a href="#" class="ticketCard-name">${item.name}</a>
-            </h3>
-            <p class="ticketCard-description">
-              ${item.description}
-            </p>
-          </div>
-          <div class="ticketCard-info">
-            <p class="ticketCard-num">
-              <span><i class="fas fa-exclamation-circle"></i></span>
-              剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
-            </p>
-            <p class="ticketCard-price">
-              TWD <span id="ticketCard-price">$${item.price}</span>
-            </p>
-          </div>
-        </div>
-      </li>
-  `;
-  });
-  cardList.innerHTML = card; // 初始渲染頁面
-  searchResult.textContent = `本次搜尋共 ${data.length} 筆資料`
+renderCard();
+function renderCard() { // 渲染資料
+  let data;
+  axios.get(dataUrl)
+    .then((res) => {
+      let card = "";
+      data = res.data.data;
+      data.forEach((item) => {
+        card += `
+          <li class="ticketCard">
+            <div class="ticketCard-img">
+              <a href="#">
+                <img
+                  src="${item.imgUrl}"
+                  alt=""
+                />
+              </a>
+              <div class="ticketCard-region">${item.area}</div>
+              <div class="ticketCard-rank">${item.rate}</div>
+            </div>
+            <div class="ticketCard-content">
+              <div>
+                <h3>
+                  <a href="#" class="ticketCard-name">${item.name}</a>
+                </h3>
+                <p class="ticketCard-description">
+                  ${item.description}
+                </p>
+              </div>
+              <div class="ticketCard-info">
+                <p class="ticketCard-num">
+                  <span><i class="fas fa-exclamation-circle"></i></span>
+                  剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+                </p>
+                <p class="ticketCard-price">
+                  TWD <span id="ticketCard-price">$${item.price}</span>
+                </p>
+              </div>
+            </div>
+          </li>
+          `;
+      });
+      cardList.innerHTML = card; // 初始渲染頁面
+      searchResult.textContent = `本次搜尋共 ${data.length} 筆資料`
+    }).catch((err) => {
+      console.log(err);
+    })
 };
 function calcText(obj) { // 計算敘述欄位字數
   let textLength = 0;
@@ -120,6 +98,107 @@ function noResult() { // 顯示 "查無關鍵字區塊"
   searchResult.textContent = `本次搜尋共 0 筆資料`
   formEl.reset();
 };
+function addCard(data) {
+  // data 已由 addChart() push 過 obj 了
+  let originalData;
+  axios.get(dataUrl)
+    .then((res) => {
+      let card = "";
+      originalData = res.data.data;
+      data.forEach((item) => {
+        originalData.push(item)
+      });
+      originalData.forEach((item) => {
+        card += `
+          <li class="ticketCard">
+            <div class="ticketCard-img">
+              <a href="#">
+                <img
+                  src="${item.imgUrl}"
+                  alt=""
+                />
+              </a>
+              <div class="ticketCard-region">${item.area}</div>
+              <div class="ticketCard-rank">${item.rate}</div>
+            </div>
+            <div class="ticketCard-content">
+              <div>
+                <h3>
+                  <a href="#" class="ticketCard-name">${item.name}</a>
+                </h3>
+                <p class="ticketCard-description">
+                  ${item.description}
+                </p>
+              </div>
+              <div class="ticketCard-info">
+                <p class="ticketCard-num">
+                  <span><i class="fas fa-exclamation-circle"></i></span>
+                  剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+                </p>
+                <p class="ticketCard-price">
+                  TWD <span id="ticketCard-price">$${item.price}</span>
+                </p>
+              </div>
+            </div>
+          </li>
+          `;
+      });
+      cardList.innerHTML = card; // 初始渲染頁面
+      searchResult.textContent = `本次搜尋共 ${originalData.length} 筆資料`
+    }).catch((err) => {
+      console.log(err);
+    })
+}
+function getData(selected) {
+  let data;
+  axios.get(dataUrl)
+    .then((res) => {
+      let card = "";
+      data = res.data.data;
+      data.forEach((item) => {
+        if (item.area === selected) {
+          console.log(item.area);
+        }
+      })
+      // data.forEach((item) => {
+      //   card += `
+      //     <li class="ticketCard">
+      //       <div class="ticketCard-img">
+      //         <a href="#">
+      //           <img
+      //             src="${item.imgUrl}"
+      //             alt=""
+      //           />
+      //         </a>
+      //         <div class="ticketCard-region">${item.area}</div>
+      //         <div class="ticketCard-rank">${item.rate}</div>
+      //       </div>
+      //       <div class="ticketCard-content">
+      //         <div>
+      //           <h3>
+      //             <a href="#" class="ticketCard-name">${item.name}</a>
+      //           </h3>
+      //           <p class="ticketCard-description">
+      //             ${item.description}
+      //           </p>
+      //         </div>
+      //         <div class="ticketCard-info">
+      //           <p class="ticketCard-num">
+      //             <span><i class="fas fa-exclamation-circle"></i></span>
+      //             剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+      //           </p>
+      //           <p class="ticketCard-price">
+      //             TWD <span id="ticketCard-price">$${item.price}</span>
+      //           </p>
+      //         </div>
+      //       </div>
+      //     </li>
+      //     `;
+      // });
+    }).catch((err) => {
+      console.log(err);
+    })
+}
 
 addTicketBtn.addEventListener('click', () => {
   let obj = {};
@@ -138,7 +217,9 @@ addTicketBtn.addEventListener('click', () => {
   if (isNone === false) { // 判斷表單內容是否都有值
     if (obj.rate <= 10 && obj.rate > 0) { // 判斷星級是不是 1 ~ 10
       if (calcText(obj) <= 100) { // 敘述要小於 100 字
-        data.push(obj);
+        id++;
+        addChart(data, obj);
+        addCard(data);
       } else {
         noResult()
         return alert(`敘述欄位字數 ${obj.description.length}，超過 100 字，請重新輸入。`);
@@ -152,12 +233,6 @@ addTicketBtn.addEventListener('click', () => {
     return alert(`欄位不得為空，請重新輸入。`);
   };
 
-
-  id++;
-
-  card = ""; // 清空初始渲染產生的卡片資料
-
-  renderCard(data);
   formEl.reset(); // 清除 form 資料
 });
 
@@ -165,7 +240,7 @@ regionSearch.addEventListener('click', (e) => {
   // 取得 select 標籤當前的值
   const val = e.target.value
   if (val === "" || val === "地區搜尋") {
-    renderCard(data);
+    renderCard();
     noneArea.classList.remove('display-block'); // 隱藏"查無關鍵字區塊"
   } else {
     // 如果 val 的值為某某地區，則渲染該地區的卡片
@@ -173,13 +248,14 @@ regionSearch.addEventListener('click', (e) => {
     // 放暫存資料的變數，用來放被篩選過的資料
     noneArea.classList.remove('display-block'); // 隱藏"查無關鍵字區塊"
     let tempData = [];
-    data.forEach((item) => { // 對 data 陣列逐一取資料
-      if (item.area === val) { // 判斷 select 的值有沒有跟陣列資料相符
-        tempData.push(item)
-      };
-    });
+    getData(val);
+    // data.forEach((item) => { // 對 data 陣列逐一取資料
+    //   if (item.area === val) { // 判斷 select 的值有沒有跟陣列資料相符
+    //     tempData.push(item)
+    //   };
+    // });
 
     // 渲染被篩選過的資料
-    renderCard(tempData);
+    // renderCard(tempData);
   }
 });
--- js/chart.js
+++ js/chart.js
@@ -1,9 +1,64 @@
-var chart = c3.generate({
-  bindto: '#chart',
-  data: {
-    columns: [
-      ['data1', 30, 200, 100, 400, 150, 250],
-      ['data2', 50, 20, 10, 40, 15, 25]
-    ]
-  }
-});
+import axios from '../node_modules/axios/dist/esm/axios.js';
+const dataUrl = "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
+
+function getChart() {
+  let data;
+  axios.get(dataUrl)
+    .then((res) => {
+      data = res.data.data;
+      const chart = c3.generate({
+        bindto: "#chart",
+        data: {
+          columns: reBuildArr(data),
+          type: 'donut',
+        },
+        donut: {
+          title: "套票地區比重"
+        }
+      });
+    }).catch((err) => {
+      console.log(err);
+    })
+};
+function reBuildArr(data) {
+  const newObj = data.reduce((acc, obj) => {
+    acc[obj.area] ?
+      acc[obj.area] += 1 :
+      acc[obj.area] = 1
+    return acc
+  }, {})
+
+  let newArr = [];
+
+  Object.keys(newObj).forEach((item) => {
+    let arr = [];
+    arr.push(item);
+    arr.push(newObj[item]);
+    newArr.push(arr);
+  });
+  return newArr
+};
+export function addChart(data, obj) {
+  data.push(obj);
+
+  let originalData;
+  axios.get(dataUrl)
+    .then((res) => {
+      originalData = res.data.data;
+      data.forEach((item) => {
+        originalData.push(item)
+      });
+      const chart = c3.generate({
+        bindto: "#chart",
+        data: {
+          columns: reBuildArr(originalData),
+          type: 'donut',
+        },
+        donut: {
+          title: "套票地區比重"
+        }
+      });
+    })
+};
+
+getChart();
